90f4284bc25b7a11e4dcc13c7fdd1ef17be304ca
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-01-29 21:00:00 +0200

    HW 8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+console.log(galleryItems);
+
+const galleryListRefs = document.querySelector('.gallery');
+
+const markapList = galleryItems
+  .map(img => {
+    return `<a class="gallery__item" href="${img.original}">
+    <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+    </a>`;
+    })
+    .join('');
+  
+galleryListRefs.innerHTML = markapList;
+galleryListRefs.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
